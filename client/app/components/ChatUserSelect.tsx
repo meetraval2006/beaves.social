33b937b5b8b69b407a7a53f1d69696a9e1c1ac48
Kishore on HP <72256091+multiii@users.noreply.github.com>
2024-11-01 18:54:30 -0700

    Add user chats (hardcoded)

---
+++ client/app/components/ChatUserSelect.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface Options {
+  id: number
+}
+
+export default function ChatUserSelect(options: Options) {
+  const router = useRouter();
+  const handleUserClick = (_: any, id: number): Promise<boolean> => router.push(`/you/chats/${id}`);
+
+  return (
+    <li>
+      <a style={{cursor: "pointer"}} onClick={(e) => handleUserClick(e, options.id)} className="flex items-center p-2 h-16 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <img src="https://i.pinimg.com/236x/68/31/12/68311248ba2f6e0ba94ff6da62eac9f6.jpg" className="flex items-center float-left h-11 w-11 mr-5 ms-2 rounded-full" alt="server-icon"/>
+        <div className="rows-2">
+
+          <div className="text-base">
+            <span>User #{options.id}</span>
+          </div>
+
+          <div className="text-sm text-gray-400">
+            <span>You: Hello, World!</span>
+          </div>
+
+        </div>
+      </a>
+    </li>
+  )
+}
--- client/app/you/chats/[id]/page.tsx
+++ client/app/you/chats/[id]/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+
 import ChatUserSelect from "@/app/components/ChatUserSelect";
 
 export default function Home() {
+  const pathname = usePathname();
+
   return (
     <>
     <a data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="a" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
@@ -46,20 +52,48 @@ export default function Home() {
         </div>
     </aside>
 
-    <div className="p-4 sm:ml-96 flex h-screen">
-        <div className="text-center m-auto">
-            <div>
-                <span className="inline-flex items-center justify-center w-[72px] h-[72px] bg-blue-600 rounded-full mb-4">
-                    <svg className="w-9 h-9 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 18" fill="currentColor">
-                        <path d="M18 4H16V9C16 10.0609 15.5786 11.0783 14.8284 11.8284C14.0783 12.5786 13.0609 13 12 13H9L6.846 14.615C7.17993 14.8628 7.58418 14.9977 8 15H11.667L15.4 17.8C15.5731 17.9298 15.7836 18 16 18C16.2652 18 16.5196 17.8946 16.7071 17.7071C16.8946 17.5196 17 17.2652 17 17V15H18C18.5304 15 19.0391 14.7893 19.4142 14.4142C19.7893 14.0391 20 13.5304 20 13V6C20 5.46957 19.7893 4.96086 19.4142 4.58579C19.0391 4.21071 18.5304 4 18 4Z" fill="currentColor"/>
-                        <path d="M12 0H2C1.46957 0 0.960859 0.210714 0.585786 0.585786C0.210714 0.960859 0 1.46957 0 2V9C0 9.53043 0.210714 10.0391 0.585786 10.4142C0.960859 10.7893 1.46957 11 2 11H3V13C3 13.1857 3.05171 13.3678 3.14935 13.5257C3.24698 13.6837 3.38668 13.8114 3.55279 13.8944C3.71889 13.9775 3.90484 14.0126 4.08981 13.996C4.27477 13.9793 4.45143 13.9114 4.6 13.8L8.333 11H12C12.5304 11 13.0391 10.7893 13.4142 10.4142C13.7893 10.0391 14 9.53043 14 9V2C14 1.46957 13.7893 0.960859 13.4142 0.585786C13.0391 0.210714 12.5304 0 12 0Z" fill="currentColor"/>
-                        </svg>
-                    <span className="sr-only">Message icon</span>
-                </span>
-            </div>
-            <div className="text-2xl font-bold mb-1">Your messages</div>
-            <div className="text-lg">Tap on a chat to continue conversing.</div>
+    <div className="sm:ml-96 flex flex-col h-screen">
+      <div className="basis-1/12 border-b border-b-indigo-200 pl-6 py-4">
+        <div className="flex items-center">
+          <img src="https://i.pinimg.com/236x/68/31/12/68311248ba2f6e0ba94ff6da62eac9f6.jpg" className="flex items-center float-left h-11 w-11 mr-5 ms-2 rounded-full" alt="server-icon"/>
+          <div className="text-xl font-semibold">User #{pathname.split("/")[3]}</div>
         </div>
+      </div>
+
+      <div className="basis-10/12 p-4 flex flex-col justify-end">
+        <div className="p-2">
+          <div className="float-right bg-indigo-500 py-2 px-4 rounded-full">Hi</div>
+        </div>
+        <div className="p-2">
+          <div className="float-right bg-indigo-500 py-2 px-4 rounded-full">How are you</div>
+        </div>
+        <div className="p-2">
+          <div className="float-right bg-indigo-500 py-2 px-4 rounded-full">My name is Foo Bar</div>
+        </div>
+        <div className="p-2">
+          <img src="https://i.pinimg.com/236x/68/31/12/68311248ba2f6e0ba94ff6da62eac9f6.jpg" className="flex items-center float-left h-11 w-11 mr-5 ms-2 rounded-full" alt="server-icon"/>
+          <div className="float-left bg-gray-500 py-2 px-4 rounded-full">Hello!</div>
+        </div>
+        <div className="p-2">
+          <img src="https://i.pinimg.com/236x/68/31/12/68311248ba2f6e0ba94ff6da62eac9f6.jpg" className="flex items-center float-left h-11 w-11 mr-5 ms-2 rounded-full" alt="server-icon"/>
+          <div className="float-left bg-gray-500 py-2 px-4 rounded-full">I'm doing great</div>
+        </div>
+        <div className="p-2">
+          <img src="https://i.pinimg.com/236x/68/31/12/68311248ba2f6e0ba94ff6da62eac9f6.jpg" className="flex items-center float-left h-11 w-11 mr-5 ms-2 rounded-full" alt="server-icon"/>
+          <div className="float-left bg-gray-500 py-2 px-4 rounded-full">My name is Bar Foo</div>
+        </div>
+      </div>
+
+      <div className="basis-1/12">
+        <div className="py-4">
+              <form className="max-w mx-12">   
+                  <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                  <div className="relative">
+                      <input type="message" className="block w-full px-4 py-2 ps-4 text-md text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Message..." required />
+                  </div>
+              </form>
+          </div>
+      </div>
     </div>
 
     </>
