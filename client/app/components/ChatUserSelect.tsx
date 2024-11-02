5fa95df906be2aae6ba1236d031d8fc23a5f8496
Kishore on HP <72256091+multiii@users.noreply.github.com>
2024-11-01 18:55:44 -0700

    Create file

--- client/app/components/ChatUserSelect.tsx
+++ client/app/components/ChatUserSelect.tsx
@@ -15,7 +15,6 @@ export default function ChatUserSelect(options: Options) {
       <a style={{cursor: "pointer"}} onClick={(e) => handleUserClick(e, options.id)} className="flex items-center p-2 h-16 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
         <img src="https://i.pinimg.com/236x/68/31/12/68311248ba2f6e0ba94ff6da62eac9f6.jpg" className="flex items-center float-left h-11 w-11 mr-5 ms-2 rounded-full" alt="server-icon"/>
         <div className="rows-2">
-
           <div className="text-base">
             <span>User #{options.id}</span>
           </div>
@@ -23,7 +22,6 @@ export default function ChatUserSelect(options: Options) {
           <div className="text-sm text-gray-400">
             <span>You: Hello, World!</span>
           </div>
-
         </div>
       </a>
     </li>
