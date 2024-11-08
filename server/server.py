23dc0ec2ba3bd840ef49e7f626a99746021909ab
Srivats720 <srivatsav.sr2@gmail.com>
2024-11-08 09:51:30 -0800

    Database

--- server/server.py
+++ server/server.py
@@ -7,11 +7,18 @@ from flask_restful import Api, Resource, reqparse, abort
 app = Flask(__name__)
 api = Api(app)
 
-cred = credentials.Certificate('firebase_private.json')
+cred = credentials.Certificate('./server/service_account_key.json')
 firebase_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-users = db.collection("users")
+users = db.collection("users").stream()
+next_id = f"{int([doc.id for doc in users][-1]) + 1}"
+
+name = input("Enter your name: ")
+pfp = input("Enter a number(place holder for pfp): ")
+role = input("What's your major: ")
+
+doc_ref = db.collection("users").document(next_id).set({"name": name, "pfp": pfp, "role": role})
 
 if __name__ == "__main__":
     app.run(debug=True)
