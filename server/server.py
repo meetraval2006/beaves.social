4811f15f3cdd4f25c93abb8c10b2a116871d37bb
Srivats720 <srivatsav.sr2@gmail.com>
2024-11-09 22:48:20 -0800

    Created create_user() function and started on get_user() but incomplete

--- server/server.py
+++ server/server.py
@@ -4,21 +4,38 @@ from firebase_admin import credentials, firestore
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
 
+import uuid
+
+def generate_random_id():
+    return str(uuid.uuid4())
+random_id = generate_random_id()
+
 app = Flask(__name__)
 api = Api(app)
 
-cred = credentials.Certificate('./server/service_account_key.json')
-firebase_app = firebase_admin.initialize_app(cred)
-
+cred = credentials.Certificate('./server/key.json')
+firebase_initialization = firebase_admin.initialize_app(cred)
 db = firestore.client()
-users = db.collection("users").stream()
-next_id = f"{int([doc.id for doc in users][-1]) + 1}"
 
-name = input("Enter your name: ")
-pfp = input("Enter a number for pfp(place holder): ")
-role = input("What's your major: ")
 
-doc_ref = db.collection("users").document(next_id).set({"name": name, "pfp": pfp, "role": role})
+def create_user():
+    #users = db.collection("users").stream()
+    name = input("Enter your name: ")
+    pfp = input("Enter a number(place holder for pfp): ")
+    role = input("What's your major: ")
+    email = input("Enter your OSU email: ")
+    doc_ref = db.collection("users").document(random_id).set({"name": name, "pfp": pfp, "role": role, "email": email})
+    return doc_ref
+
+#get_user() is incomplete. 
+def get_user():
+    name = db.collection("users").document(random_id).get().to_dict()["name"]
+    role = db.collection("users").document(random_id).get().to_dict()["role"]
+    email = db.collection("users").document(random_id).get().to_dict()["email"]
+    user_info = {name, role, email}
+    return user_info
+
+create_user()
 
 if __name__ == "__main__":
     app.run(debug=True)
