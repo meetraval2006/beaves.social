112310d4e1f9212501baf53585567bba00972320
meet.raval <meetnraval@gmail.com>
2024-11-30 11:30:21 -0800

    .

--- server/create_events.py
+++ server/create_events.py
@@ -14,51 +14,6 @@ cred = credentials.Certificate('./server/key.json')
 firebase_initialization = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-@app.route('/api/create_user', methods=['POST'])
-def create_user():
-    try:
-        data = request.get_json()
-        name = data.get("name")
-        username = data.get("username")
-        major = data.get("major")
-        minor = data.get("minor")
-        year = data.get("year")
-        resident_hall = data.get("resident_hall")
-        email = data.get("email")
-
-        if not re.match(r"^[a-zA-Z]+", name):
-            abort(400, message="Invalid name. Please type your full name")
-
-        if not re.match(r"^[a-zA-Z0-9_$#!.,:><%-=+]+", username):
-            abort(400, message="Invalid username. Please type a valid username")
-
-        if not re.match(r"^[a-zA-Z0-9]+@oregonstate.edu", email):
-            abort(400, message="Invalid email. Please type a valid Oregon State email")
-
-        if not name or not username or not major or not minor or not year or not resident_hall or not email:
-            abort(400, message="Please provide all the information")
-        
-        random_id = str(uuid.uuid4())
-
-        db.collection("users").document(random_id).set({
-            "name": name,
-            "username": username,
-            "major": major,
-            "minor": minor,
-            "year": year,
-            "resident_hall": resident_hall,
-            "email": email
-        })
-
-        return jsonify({
-            "id": random_id, "name": name, "username": username, 
-            "major": major, "minor": minor, "year": year, 
-            "resident_hall": resident_hall, "email": email
-        })
-    
-    except Exception as e:
-        abort(400, message=f"Error creating user: {e}")
-
 
 @app.route('/api/get_user', methods=['GET'])
 def get_user():
