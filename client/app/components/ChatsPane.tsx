57a20f309991fee20abeef46daa72cda907e57c7
Srivats720 <srivatsav.sr2@gmail.com>
2024-12-02 10:14:01 -0800

    Front end change 4

--- client/app/components/ChatsPane.tsx
+++ client/app/components/ChatsPane.tsx
@@ -24,7 +24,7 @@ export default function ChatsPane() {
     <div className="px-2 pb-4 pt-2 overflow-y-auto">
       <ul className="font-medium">
         {Object.keys(data).map((key) => {
-          return <ChatUserSelect key={key} username={data[key].gc_name} latestMessageAuthor={data[key].messages?.length ? "0" : ""} latestMessageText={data[key].messages?.length ? "0" : ""} isMessage={data[key].messages?.length} />
+          return <ChatUserSelect key={key} id={key} username={data[key].gc_name} latestMessageAuthor={data[key].messages?.length ? "0" : ""} latestMessageText={data[key].messages?.length ? "0" : ""} isMessage={data[key].messages?.length} />
         })}
     </ul>
     </div>
